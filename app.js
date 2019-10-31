8e1204de2f038aa7d4080a8fbab862b08e83019b
Aviva Weinbaum <aw3156@barnard.edu>
2019-10-31 14:16:41 -0400

    added some js

--- app.js
+++ app.js
@@ -5,8 +5,9 @@ function processTerms() {
   console.log("made it here");
   text = document.getElementById('terms').value;
   console.log(text);
-  document.getElementById('title').innerHTML = 'This policy includes:\n';
-  document.getElementById('output').innerHTML = 'Third parties.....';
-  //document.getElementById('output').innerHTML = 'This policy includes: ' + text;
+  document.getElementById('title').innerHTML = 'Key terms in this  policy:\n';
+  //document.getElementById('output').innerHTML = '<b>Third-parties</b>: Other parties may be receiving your data without your knowledge or consent.<br>';
+  //document.getElementById('output').innerHTML += '<b>Identify</b>: Your information may or may not be identifiable to you. See <i>Protecting your Privacy</i> for more information.';
+  document.getElementById('output').innerHTML = text + ':\n';
 }
 
