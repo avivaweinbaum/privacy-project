6106bc6a375e625bb679b81c116c0a57f7c9e99d
Aviva Weinbaum <aw3156@barnard.edu>
2019-10-31 00:13:21 -0400

    added nav bar and edits

--- app.js
+++ app.js
@@ -1,6 +1,12 @@
+
+
+
 function processTerms() {
   console.log("made it here");
   text = document.getElementById('terms').value;
   console.log(text);
+  document.getElementById('title').innerHTML = 'This policy includes:\n';
+  document.getElementById('output').innerHTML = 'Third parties.....';
+  //document.getElementById('output').innerHTML = 'This policy includes: ' + text;
 }
 
