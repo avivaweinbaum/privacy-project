51b3e93b94454110cb5de2bcb2b3803875779348
Aviva Weinbaum <aw3156@barnard.edu>
2019-11-18 14:17:44 -0500

    added js parsing

--- app.js
+++ app.js
@@ -1,13 +1,45 @@
-
-
+var keyWords = [
+	"third",
+	"party",
+	"affiliates",
+	"advertisers",
+	"advertiser",
+	"advertisement",
+	"consent",
+	"cookies",
+	"liable",
+	"liability",
+	"data",
+	"automatic",
+	"renewal",
+	"discretion",
+	"location",
+	"tracking",
+	"track",
+	"information"
+];
 
 function processTerms() {
-  console.log("made it here");
+  //console.log("made it here");
+  document.getElementById('output').innerHTML = ""; 
+  var report = [];
   text = document.getElementById('terms').value;
+  text = text.split(" ");
   console.log(text);
+  
+  for (var i = 0; i < text.length; i++) {
+	if (keyWords.includes(text[i]) && !report.includes(text[i])) {
+	  report.push(text[i]);
+	}
+  }
+
   document.getElementById('title').innerHTML = 'Key terms in this  policy:\n';
+  
   //document.getElementById('output').innerHTML = '<b>Third-parties</b>: Other parties may be receiving your data without your knowledge or consent.<br>';
   //document.getElementById('output').innerHTML += '<b>Identify</b>: Your information may or may not be identifiable to you. See <i>Protecting your Privacy</i> for more information.';
-  document.getElementById('output').innerHTML = text + ':\n';
+  for (var j = 0; j < report.length; j++) {
+    console.log(report[j]);
+	document.getElementById('output').innerHTML += report[j] + "<br>";
+  }
 }
 
