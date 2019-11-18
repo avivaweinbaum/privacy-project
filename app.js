66dee79c10a652f68f71d522bfea0ddb82d88551
Aviva Weinbaum <aw3156@barnard.edu>
2019-11-18 14:22:23 -0500

    minor changes to parsing

--- app.js
+++ app.js
@@ -32,9 +32,12 @@ function processTerms() {
 	  report.push(text[i]);
 	}
   }
-
-  document.getElementById('title').innerHTML = 'Key terms in this  policy:\n';
-  
+  if (report.length == 0) {
+	 document.getElementById('title').innerHTML = 'There are no key terms in this policy.';
+  }
+  else {
+    document.getElementById('title').innerHTML = 'Key terms in this  policy:\n';
+  }
   //document.getElementById('output').innerHTML = '<b>Third-parties</b>: Other parties may be receiving your data without your knowledge or consent.<br>';
   //document.getElementById('output').innerHTML += '<b>Identify</b>: Your information may or may not be identifiable to you. See <i>Protecting your Privacy</i> for more information.';
   for (var j = 0; j < report.length; j++) {
